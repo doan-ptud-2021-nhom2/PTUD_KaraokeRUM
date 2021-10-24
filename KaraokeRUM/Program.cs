97d63a1816f11bb4d8d88c35ef34381b01dfc702
Bao Tran <baotran2k1@outlook.com>
2021-10-24 17:31:48 +0700

    Quản lý phòng (thêm)

--- KaraokeRUM/Program.cs
+++ KaraokeRUM/Program.cs
@@ -16,7 +16,7 @@ namespace KaraokeRUM
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmTrangChu());
+            Application.Run(new frmDangNhap());
         }
     }
 }
