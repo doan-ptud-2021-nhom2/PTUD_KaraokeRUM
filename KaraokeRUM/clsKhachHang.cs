c34c92f9138a46542e46f8a5380ff91e93fc3742
biaoHuynh <hnquocbao22@gmail.com>
2021-10-24 16:54:15 +0700

    bao load dskh

---
+++ KaraokeRUM/clsKhachHang.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaraokeRUM
+{
+    class clsKhachHang : clsKetNoi
+    {
+        qlKaraokeDataContext dt;
+        
+        public clsKhachHang()
+        {
+            dt = LayData();
+        }
+        //Hàm lấy thông tin khách hàng
+        public List<KhachHang> LayDSKH()
+        {
+
+            var dataKH = from kh in dt.KhachHangs
+                         select new
+                         {
+                             ten = kh.TenKhach,
+                             makh = kh.MaKH,
+                             sld = kh.SoLanDen,
+                             sdt = kh.SDT
+                         };
+            List<KhachHang> lKH = new List<KhachHang>();
+            foreach(var data in dataKH)
+            {
+                KhachHang kh = new KhachHang()
+                {
+                    TenKhach = data.ten,
+                    MaKH = data.makh,
+                    SDT = data.sdt,
+                    SoLanDen = data.sld
+                }; 
+                lKH.Add(kh);
+            }
+            return lKH;
+        }
+    }
+}
--- KaraokeRUM/frmQuanLyKhachHang.cs
+++ KaraokeRUM/frmQuanLyKhachHang.cs
@@ -19,23 +19,16 @@ namespace KaraokeRUM
 
         private void frmQuanLyKhachHang_Load(object sender, EventArgs e)
         {
-            var dbkh = new clsKetNoi().LayData();
-            var data = from kh in dbkh.KhachHangs
-                       select new
-                       {
-                           ten = kh.TenKhach,
-                           makh = kh.MaKH,
-                           sld = kh.SoLanDen,
-                           sdt = kh.SDT
-                       };
-            foreach(var kh in data) {
+            var dskh = new clsKhachHang().LayDSKH();
+            foreach (var kh in dskh) {
                 this.lvwDSKH.View = View.Details;             
                 lvwDSKH.BeginUpdate();
-                string[] row = { kh.ten, kh.sdt, kh.makh, kh.sld.ToString() };
+                string[] row = { kh.TenKhach, kh.SDT, kh.MaKH, kh.SoLanDen.ToString() };
                 var listViewItem = new ListViewItem(row);
                 this.lvwDSKH.Items.Add(listViewItem);
                 lvwDSKH.EndUpdate();               
             }
+            lvwDSKH.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);// tự động căng theo chiều rộng nội dung trong column
         }
     }
 }
