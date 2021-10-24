3b2e69762128b2af6051ecd779aae86ffc32414c
biaoHuynh <hnquocbao22@gmail.com>
2021-10-24 13:36:35 +0700

    Up lên lvw KhachHang

--- KaraokeRUM/frmQuanLyKhachHang.cs
+++ KaraokeRUM/frmQuanLyKhachHang.cs
@@ -19,7 +19,23 @@ namespace KaraokeRUM
 
         private void frmQuanLyKhachHang_Load(object sender, EventArgs e)
         {
-           
+            var dbkh = new clsKetNoi().LayData();
+            var data = from kh in dbkh.KhachHangs
+                       select new
+                       {
+                           ten = kh.TenKhach,
+                           makh = kh.MaKH,
+                           sld = kh.SoLanDen,
+                           sdt = kh.SDT
+                       };
+            foreach(var kh in data) {
+                this.lvwDSKH.View = View.Details;             
+                lvwDSKH.BeginUpdate();
+                string[] row = { kh.ten, kh.sdt, kh.makh, kh.sld.ToString() };
+                var listViewItem = new ListViewItem(row);
+                this.lvwDSKH.Items.Add(listViewItem);
+                lvwDSKH.EndUpdate();               
+            }
         }
     }
 }
