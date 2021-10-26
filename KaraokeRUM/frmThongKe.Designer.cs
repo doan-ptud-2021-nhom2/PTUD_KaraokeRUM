a16e341248b5426e77af534f821f7cf060db105b
iamtun <letuan19431791.iuh@gmail.com>
2021-10-26 16:04:40 +0700

    abc

--- KaraokeRUM/frmThongKe.Designer.cs
+++ KaraokeRUM/frmThongKe.Designer.cs
@@ -38,20 +38,20 @@ namespace KaraokeRUM
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.lvwDanhSach = new System.Windows.Forms.ListView();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cboNam = new System.Windows.Forms.ComboBox();
+            this.cboThang = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.btnTKDoanhThu = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtSMH = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTDT = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSKH = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.cboThang = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.cboNam = new System.Windows.Forms.ComboBox();
-            this.txtSKH = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.txtTDT = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.txtSMH = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -66,8 +66,9 @@ namespace KaraokeRUM
             this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label11.ForeColor = System.Drawing.SystemColors.Window;
             this.label11.Location = new System.Drawing.Point(0, 0);
+            this.label11.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(1649, 50);
+            this.label11.Size = new System.Drawing.Size(1237, 41);
             this.label11.TabIndex = 3;
             this.label11.Text = "Thống kê";
             this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -80,9 +81,11 @@ namespace KaraokeRUM
             // 
             this.groupBox1.Controls.Add(this.lvwDanhSach);
             this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(24, 92);
+            this.groupBox1.Location = new System.Drawing.Point(18, 75);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(777, 300);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(583, 244);
             this.groupBox1.TabIndex = 82;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Danh Sách";
@@ -90,9 +93,10 @@ namespace KaraokeRUM
             // lvwDanhSach
             // 
             this.lvwDanhSach.HideSelection = false;
-            this.lvwDanhSach.Location = new System.Drawing.Point(6, 43);
+            this.lvwDanhSach.Location = new System.Drawing.Point(4, 35);
+            this.lvwDanhSach.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lvwDanhSach.Name = "lvwDanhSach";
-            this.lvwDanhSach.Size = new System.Drawing.Size(765, 243);
+            this.lvwDanhSach.Size = new System.Drawing.Size(575, 198);
             this.lvwDanhSach.TabIndex = 0;
             this.lvwDanhSach.UseCompatibleStateImageBehavior = false;
             // 
@@ -105,20 +109,51 @@ namespace KaraokeRUM
             this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Controls.Add(this.radioButton1);
             this.groupBox2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox2.Location = new System.Drawing.Point(30, 423);
+            this.groupBox2.Location = new System.Drawing.Point(22, 344);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(345, 215);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Size = new System.Drawing.Size(259, 175);
             this.groupBox2.TabIndex = 83;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Lựa chọn";
+            // 
+            // cboNam
+            // 
+            this.cboNam.FormattingEnabled = true;
+            this.cboNam.Location = new System.Drawing.Point(81, 93);
+            this.cboNam.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cboNam.Name = "cboNam";
+            this.cboNam.Size = new System.Drawing.Size(147, 27);
+            this.cboNam.TabIndex = 4;
+            // 
+            // cboThang
+            // 
+            this.cboThang.FormattingEnabled = true;
+            this.cboThang.Location = new System.Drawing.Point(81, 58);
+            this.cboThang.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cboThang.Name = "cboThang";
+            this.cboThang.Size = new System.Drawing.Size(147, 27);
+            this.cboThang.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 96);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Năm:";
             // 
             // btnTKDoanhThu
             // 
             this.btnTKDoanhThu.BackColor = System.Drawing.Color.Teal;
             this.btnTKDoanhThu.ForeColor = System.Drawing.Color.White;
-            this.btnTKDoanhThu.Location = new System.Drawing.Point(108, 169);
+            this.btnTKDoanhThu.Location = new System.Drawing.Point(81, 137);
+            this.btnTKDoanhThu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnTKDoanhThu.Name = "btnTKDoanhThu";
-            this.btnTKDoanhThu.Size = new System.Drawing.Size(195, 40);
+            this.btnTKDoanhThu.Size = new System.Drawing.Size(146, 32);
             this.btnTKDoanhThu.TabIndex = 3;
             this.btnTKDoanhThu.Text = "Thống kê";
             this.btnTKDoanhThu.UseVisualStyleBackColor = false;
@@ -126,18 +161,20 @@ namespace KaraokeRUM
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(32, 75);
+            this.label2.Location = new System.Drawing.Point(24, 61);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 22);
+            this.label2.Size = new System.Drawing.Size(49, 19);
             this.label2.TabIndex = 1;
             this.label2.Text = "Tháng:";
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(36, 29);
+            this.radioButton1.Location = new System.Drawing.Point(27, 24);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(101, 26);
+            this.radioButton1.Size = new System.Drawing.Size(82, 23);
             this.radioButton1.TabIndex = 0;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Hôm nay";
@@ -152,19 +189,66 @@ namespace KaraokeRUM
             this.groupBox3.Controls.Add(this.txtSKH);
             this.groupBox3.Controls.Add(this.label3);
             this.groupBox3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox3.Location = new System.Drawing.Point(450, 423);
+            this.groupBox3.Location = new System.Drawing.Point(338, 344);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(345, 215);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox3.Size = new System.Drawing.Size(259, 175);
             this.groupBox3.TabIndex = 84;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Thông tin";
             // 
+            // txtSMH
+            // 
+            this.txtSMH.Location = new System.Drawing.Point(114, 89);
+            this.txtSMH.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtSMH.Name = "txtSMH";
+            this.txtSMH.Size = new System.Drawing.Size(126, 26);
+            this.txtSMH.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 91);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 19);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Số mặt hàng:";
+            // 
+            // txtTDT
+            // 
+            this.txtTDT.Location = new System.Drawing.Point(114, 123);
+            this.txtTDT.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTDT.Name = "txtTDT";
+            this.txtTDT.Size = new System.Drawing.Size(126, 26);
+            this.txtTDT.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 125);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 19);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Tổng doanh thu:";
+            // 
+            // txtSKH
+            // 
+            this.txtSKH.Location = new System.Drawing.Point(114, 58);
+            this.txtSKH.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtSKH.Name = "txtSKH";
+            this.txtSKH.Size = new System.Drawing.Size(126, 26);
+            this.txtSKH.TabIndex = 5;
+            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 74);
+            this.label3.Location = new System.Drawing.Point(12, 60);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(128, 22);
+            this.label3.Size = new System.Drawing.Size(101, 19);
             this.label3.TabIndex = 2;
             this.label3.Text = "Số khách hàng:";
             // 
@@ -175,93 +259,31 @@ namespace KaraokeRUM
             this.chart1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(863, 92);
+            this.chart1.Location = new System.Drawing.Point(647, 75);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.chart1.Name = "chart1";
             this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
             series1.ChartArea = "ChartArea1";
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(774, 530);
+            this.chart1.Size = new System.Drawing.Size(580, 431);
             this.chart1.TabIndex = 86;
             this.chart1.Text = "chart1";
             // 
-            // cboThang
-            // 
-            this.cboThang.FormattingEnabled = true;
-            this.cboThang.Location = new System.Drawing.Point(108, 72);
-            this.cboThang.Name = "cboThang";
-            this.cboThang.Size = new System.Drawing.Size(195, 30);
-            this.cboThang.TabIndex = 4;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(32, 118);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 22);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Năm:";
-            // 
-            // cboNam
-            // 
-            this.cboNam.FormattingEnabled = true;
-            this.cboNam.Location = new System.Drawing.Point(108, 115);
-            this.cboNam.Name = "cboNam";
-            this.cboNam.Size = new System.Drawing.Size(195, 30);
-            this.cboNam.TabIndex = 4;
-            // 
-            // txtSKH
-            // 
-            this.txtSKH.Location = new System.Drawing.Point(152, 71);
-            this.txtSKH.Name = "txtSKH";
-            this.txtSKH.Size = new System.Drawing.Size(167, 30);
-            this.txtSKH.TabIndex = 5;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(16, 154);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(136, 22);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "Tổng doanh thu:";
-            // 
-            // txtTDT
-            // 
-            this.txtTDT.Location = new System.Drawing.Point(152, 151);
-            this.txtTDT.Name = "txtTDT";
-            this.txtTDT.Size = new System.Drawing.Size(167, 30);
-            this.txtTDT.TabIndex = 5;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(16, 112);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(111, 22);
-            this.label5.TabIndex = 2;
-            this.label5.Text = "Số mặt hàng:";
-            // 
-            // txtSMH
-            // 
-            this.txtSMH.Location = new System.Drawing.Point(152, 109);
-            this.txtSMH.Name = "txtSMH";
-            this.txtSMH.Size = new System.Drawing.Size(167, 30);
-            this.txtSMH.TabIndex = 5;
-            // 
             // frmThongKe
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1649, 650);
+            this.ClientSize = new System.Drawing.Size(1237, 528);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label11);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmThongKe";
             this.Text = "Form9";
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
