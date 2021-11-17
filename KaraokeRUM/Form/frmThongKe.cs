970b562782afd70d9cb9148abaf6e202d28d87db
iamtun <letuan19431791.iuh@gmail.com>
2021-11-17 15:48:15 +0700

    Them Chuc Nang Xuat Excel

--- KaraokeRUM/Form/frmThongKe.cs
+++ KaraokeRUM/Form/frmThongKe.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Excel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -123,11 +124,7 @@ namespace KaraokeRUM
             //Chạy vòng lặp để show dữ liệu
             foreach (dynamic item in data)
             {
-                //Đối với các loại nước có đơn vị là lon thì / 24 để tính bằng thùng
-                if(MATHANG.LayDonViMatHang(item.MatHang).DonVi.Equals("Lon"))
-                    chrThongKeDoanhThu.Series["MatHang"].Points.AddXY(item.MatHang, item.SoLuong / 24);
-                else
-                    chrThongKeDoanhThu.Series["MatHang"].Points.AddXY(item.MatHang, item.SoLuong);
+                chrThongKeDoanhThu.Series["MatHang"].Points.AddXY(item.MatHang, item.SoLuong);
             }
         }
 
@@ -138,10 +135,7 @@ namespace KaraokeRUM
             chrThongKeDoanhThu.Series["MatHang"].IsValueShownAsLabel = true;
             foreach (dynamic item in data)
             {
-                if (MATHANG.LayDonViMatHang(item.MatHang).DonVi.Equals("Lon"))
-                    chrThongKeDoanhThu.Series["MatHang"].Points.AddXY(item.MatHang, (item.SoLuong / 24));
-                else
-                    chrThongKeDoanhThu.Series["MatHang"].Points.AddXY(item.MatHang, item.SoLuong);
+                chrThongKeDoanhThu.Series["MatHang"].Points.AddXY(item.MatHang, item.SoLuong);
             }
         }
 
@@ -234,6 +228,51 @@ namespace KaraokeRUM
             //MessageBox.Show(maHD);
             frmHoaDon frm = new frmHoaDon(maHD, statusBtn, null);
             frm.Show();
+        }
+
+        private void btnXuatExcel_Click(object sender, EventArgs e)
+        {
+            if(lstvDSHoaDon.Items.Count == 0)
+            {
+                MessageBox.Show("Không có dữ liệu để xuất!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                using (SaveFileDialog sdf = new SaveFileDialog() { Filter = "Excel work|*.xls", ValidateNames = true })
+                {
+                    if (sdf.ShowDialog() == DialogResult.OK)
+                    {
+                        Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+                        Workbook wb = app.Workbooks.Add(XlSheetType.xlWorksheet);
+                        Worksheet ws = (Worksheet)app.ActiveSheet;
+                        app.Visible = false;
+                        ws.Cells[1, 1] = "Mã hóa đơn";
+                        ws.Cells[1, 2] = "Tên phòng";
+                        ws.Cells[1, 3] = "Tên khách hàng";
+                        ws.Cells[1, 4] = "Tổng tiền";
+                        ws.Cells[1, 5] = "Ngày lập";
+
+                        int i = 2;
+                        foreach (ListViewItem item in lstvDSHoaDon.Items)
+                        {
+                            ws.Cells[i, 1] = item.SubItems[0].Text;
+                            ws.Cells[i, 2] = item.SubItems[1].Text;
+                            ws.Cells[i, 3] = item.SubItems[2].Text;
+                            ws.Cells[i, 4] = item.SubItems[3].Text;
+                            ws.Cells[i, 5] = item.SubItems[4].Text;
+
+                            i++;
+                        }
+
+                        wb.SaveAs(sdf.FileName, XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, true, false,
+                                  XlSaveAsAccessMode.xlNoChange, XlSaveConflictResolution.xlLocalSessionChanges, Type.Missing,
+                                  Type.Missing);
+                        app.Quit();
+                        MessageBox.Show("Xuất dữ liệu thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            
         }
     }
 }
